3ab40fcbfc5358e0c8c87187c892f9ce8dd9103a
sol <solcattaneo@gmail.com>
2021-11-09 15:52:17 -0300

    registro

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require ('express');
+const path = require ('path');
+
+const app = express ();
+const port = 3000;
+const publicPath = path.resolve (__dirname, './public' )
+app.use(express.static (publicPath))
+
+app.listen(port, () => {console.log ("Servidor corriendo en puerto" + port)});
+
+app.get ('/', function (req, res) { res.sendFile (path.join (__dirname, '/views/home.html'))} );
+
+app.get ('/register', function (req, res) { res.sendFile (path.join (__dirname, '/views/register.html'))} );
+
+app.post("/register", (req, res) => {res.redirect("/")});
+
+app.get ('/login', function (req, res) { res.sendFile (path.join (__dirname, '/views/login.html'))} );
+
+app.post("/login", (req, res) => {res.redirect("/")});
